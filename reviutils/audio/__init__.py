855209c7897a5c8b8242aa6f436edccd95f9eacb
Viyyy <1793707472@qq.com>
2024-03-20 11:22:48 +0800

    add module to init file

---
+++ reviutils/audio/__init__.py
@@ -0,0 +1,4 @@
+import reviutils.audio.clipper as clipper
+import reviutils.audio.reader as reader
+
+__all__ = ['clipper','reader']
---
+++ reviutils/noisepollution/__init__.py
@@ -0,0 +1,11 @@
+import reviutils.noisepollution.evaluation as evaluation
+import reviutils.noisepollution.funcarea as funcarea
+import reviutils.noisepollution.hourhelper as hourhelper
+import reviutils.noisepollution.splhelper as splhelper
+
+__all__ = [
+    'evaluation',
+    'funcarea',
+    'hourhelper',
+    'splhelper'
+]
